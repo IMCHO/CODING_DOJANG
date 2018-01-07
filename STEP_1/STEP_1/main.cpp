1161676789e63b5aab3d52e8af4fc8fb4fd655d2
인택 조 <dlsxor21c@naver.com>
2018-01-07 16:20:47 +0900

    Reculsive_Generator about 1

--- STEP_1/STEP_1/main.cpp
+++ STEP_1/STEP_1/main.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+
 using namespace std;
 
+void GeneratorFunc(string str);
+
+vector<int> temp;
 int main()
 {
-	string str;
+	GeneratorFunc("1");
+	
+	for (int i = 0; i < temp.size(); i++)
+	{
+		cout << temp[i] << " ";
+	}
+	
+	return 0;
+}
+
+void GeneratorFunc(string str_num)
+{
 	int sum = 0;
 	int result;
-	cout << "Input : ";
-	cin >> str;
 
-	for (int i = 0; i < str.size(); i++)
+	stringstream sstr;
+	for (int i = 0; i < str_num.size(); i++)
 	{
-		sum += (str.at(i) - '0');
+		sum += (str_num.at(i) - '0');
 	}
 
-	result = sum + stoi(str);
+	result = sum + stoi(str_num);
+	if (result < 5000)
+	{
+		temp.push_back(result);
+		sstr << result;
+		GeneratorFunc(sstr.str());
+	}
 
-	cout << result << endl;
-
-	return 0;
 }
