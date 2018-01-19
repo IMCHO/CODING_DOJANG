98d1e937bd852dfe360ad113d2e56f5ea26ba5d8
인택 조 <dlsxor21c@naver.com>
2018-01-19 23:56:03 +0900

    make spiral matrix but not complete

--- STEP_2/STEP_2/main.cpp
+++ STEP_2/STEP_2/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
+bool CheckMatrix(int * _matrix[],int _row,int _column);
+
 int main()
 {
 	int ** matrix;
 	int row, column;
 	int rtemp = 0, ctemp = 0;
 	int num = 0;
+	int level = 1;
 	cin >> row >> column;
 	
 	matrix = new int*[row];
@@ -16,26 +19,59 @@ int main()
 	for (int i = 0; i < row; i++) 
 		for (int j = 0; j < column; j++) matrix[i][j] = { -1, };
 
-	//while (ctemp<column)
-	//{
-	//	matrix[rtemp][ctemp] = num;
-	//	ctemp++;
-	//	num++;
-	//	if (ctemp == column)
-	//	{
-	//		while (rtemp < row)
-	//		{
-	//			matrix[rtemp][ctemp] = num;
-	//			rtemp++;
-	//			num++;
-	//			if (rtemp == row)
-	//			{
-
-	//			}
-	//		}
-	//	}
-	//}
+	while (!CheckMatrix(matrix, row, column))
+	{
+		if (level == 1)
+		{
+			while (rtemp < row && ctemp < column && matrix[rtemp][ctemp] == -1)
+			{
+				matrix[rtemp][ctemp] = num;
+				num++;
+				ctemp++;
+				level = 2;
+			}
+		}
+		else if (level == 2)
+		{
+			while (rtemp < row && ctemp < column && matrix[rtemp][ctemp] == -1)
+			{
+				matrix[rtemp][ctemp] = num;
+				num++;
+				rtemp++;
+				level = 3;
+			}
+		}
+		else if (level == 3)
+		{
+			while (rtemp < row && ctemp < column && matrix[rtemp][ctemp] == -1)
+			{
+				matrix[rtemp][ctemp] = num;
+				num++;
+				ctemp--;
+				level = 4;
+			}
+		}
+		else if (level == 4)
+		{
+			while (rtemp < row && ctemp < column && matrix[rtemp][ctemp] == -1)
+			{
+				matrix[rtemp][ctemp] = num;
+				num++;
+				rtemp--;
+				level = 1;
+			}
+		}
+	}
 
 
 	return 0;
+}
+
+bool CheckMatrix(int * _matrix[], int _row, int _column)
+{
+	for (int i = 0; i < _row; i++)
+		for (int j = 0; j < _column; j++)
+		{
+			if (_matrix[_row][_column] == -1) return false;
+		}
 }
