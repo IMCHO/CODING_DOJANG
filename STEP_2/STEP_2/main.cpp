8784cc3bc1ae1a82fe17acef10b45a71c3e8672a
인택 조 <dlsxor21c@naver.com>
2018-01-19 21:55:50 +0900

    construct 6x6 matrix and initialize -1

--- STEP_2/STEP_2/main.cpp
+++ STEP_2/STEP_2/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-	std::cout << "hello" << std::endl;
+	int ** matrix;
+	int row, column;
+	int rtemp = 0, ctemp = 0;
+	int num = 0;
+	cin >> row >> column;
+	
+	matrix = new int*[row];
+	for (int i = 0; i < row; i++) 
+		matrix[i] = new int[column];
+	
+	for (int i = 0; i < row; i++) 
+		for (int j = 0; j < column; j++) matrix[i][j] = { -1, };
+
+	//while (ctemp<column)
+	//{
+	//	matrix[rtemp][ctemp] = num;
+	//	ctemp++;
+	//	num++;
+	//	if (ctemp == column)
+	//	{
+	//		while (rtemp < row)
+	//		{
+	//			matrix[rtemp][ctemp] = num;
+	//			rtemp++;
+	//			num++;
+	//			if (rtemp == row)
+	//			{
+
+	//			}
+	//		}
+	//	}
+	//}
+
+
 	return 0;
 }
