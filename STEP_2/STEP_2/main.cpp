a6a97563d8465f1aec8cf61d39214a1f362e470a
인택 조 <dlsxor21c@naver.com>
2018-01-23 17:44:39 +0900

    change algorithm but not complete

--- STEP_2/STEP_2/main.cpp
+++ STEP_2/STEP_2/main.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
 
-bool CheckMatrix(int * _matrix[],int _row,int _column);
+void SpiralM(int * _matrix[],int _row,int _column);
 
 int main()
 {
 	int ** matrix;
 	int row, column;
-	int rtemp = 0, ctemp = 0;
-	int num = 0;
-	int level = 1;
 	cin >> row >> column;
 	
 	matrix = new int*[row];
@@ -18,86 +15,6 @@ int main()
 	
 	for (int i = 0; i < row; i++) 
 		for (int j = 0; j < column; j++) matrix[i][j] = -1;
-
-	while (!CheckMatrix(matrix, row, column))
-	{
-		if (level == 1)
-		{
-			while (rtemp < row && ctemp < column)
-			{
-				if (matrix[rtemp][ctemp] != -1)
-				{
-					ctemp++;
-					continue;
-				}
-				else
-				{
-					matrix[rtemp][ctemp] = num;
-					num++;
-					ctemp++;
-				}
-			}
-			level = 2;
-			ctemp-=2;
-		}
-		else if (level == 2)
-		{
-			while (rtemp < row && ctemp < column)
-			{
-				if (matrix[rtemp][ctemp] != -1)
-				{
-					rtemp++;
-					continue;
-				}
-				else
-				{
-					matrix[rtemp][ctemp] = num;
-					num++;
-					rtemp++;
-				}
-			}
-			level = 3;
-			rtemp-=2;
-		}
-		else if (level == 3)
-		{
-			while (rtemp >=0 && ctemp >= 0)
-			{
-				if (matrix[rtemp][ctemp] != -1)
-				{
-					ctemp--;
-					continue;
-				}
-				else
-				{
-					matrix[rtemp][ctemp] = num;
-					num++;
-					ctemp--;
-				}
-			}
-			level = 4;
-			ctemp+=2;
-		}
-		else if (level == 4)
-		{
-			while (rtemp >= 0 && ctemp >= 0)
-			{
-				if (matrix[rtemp][ctemp] != -1)
-				{
-					rtemp--;
-					continue;
-				}
-				else
-				{
-					matrix[rtemp][ctemp] = num;
-					num++;
-					rtemp--;
-				}
-			}
-			level = 1;
-			rtemp+=2;
-		}
-	}
 
 	for (int i = 0; i < row; i++)
 	{
@@ -112,12 +29,16 @@ int main()
 	return 0;
 }
 
-bool CheckMatrix(int * _matrix[], int _row, int _column)
+void SpiralM(int * _matrix[], int _row, int _column)
 {
-	for (int i = 0; i < _row; i++)
-		for (int j = 0; j < _column; j++)
+	int num = 0;
+	int ctemp, rtemp = 0;
+	if (_row % 2 == 0)
+	{
+		int count = _row / 2;
+		while (count != 0)
 		{
-			if (_matrix[i][j] == -1) return false;
+			
 		}
-	return true;
+	}
 }
