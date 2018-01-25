6b28d6a84e21742832bf052491feb854c44d00c3
인택 조 <dlsxor21c@naver.com>
2018-01-25 17:32:55 +0900

    v0.1(need arrangement)

--- STEP_2/STEP_2/main.cpp
+++ STEP_2/STEP_2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void SpiralM(int * _matrix[],int _row,int _column);
@@ -16,11 +17,14 @@ int main()
 	for (int i = 0; i < row; i++) 
 		for (int j = 0; j < column; j++) matrix[i][j] = -1;
 
+	SpiralM(matrix, row, column);
+
 	for (int i = 0; i < row; i++)
 	{
 		for (int j = 0; j < column; j++)
 		{
-			cout << matrix[i][j] << " ";
+			cout << setw(3);
+			cout<< matrix[i][j] << "  ";
 		}
 		cout << endl;
 	}
@@ -32,13 +36,86 @@ int main()
 void SpiralM(int * _matrix[], int _row, int _column)
 {
 	int num = 0;
-	int ctemp, rtemp = 0;
+	int temp = 0;
+	int differ = 0;
+	int final_num = _row * 4 - 5;
+	int ctemp = 0 , rtemp = 0;
 	if (_row % 2 == 0)
 	{
 		int count = _row / 2;
 		while (count != 0)
 		{
+			_matrix[rtemp][ctemp] = num;
+			_matrix[rtemp + 1][ctemp] = final_num;
+			temp = num;
+			num = final_num + 1;
+			final_num = final_num - temp - 8 + num;
+
+			for (int i = ctemp+1; i < _row - differ; i++)
+			{
+				_matrix[rtemp][i] = ++temp;
+			}
+
+			int ctemp2 = _row - differ-1;
+			for (int i = rtemp+1; i < _row-differ; i++)
+			{
+				_matrix[i][ctemp2] = ++temp;
+			}
+
+			int rtemp2 = _row - differ - 1;
+			for (int i = ctemp2 - 1; i >_row-rtemp2-2 ; i--)
+			{
+				_matrix[rtemp2][i] = ++temp;
+			}
 			
+			int ctemp3 = _row - rtemp2 - 1;
+			for (int i = rtemp2-1; i > _row-rtemp2-1; i--)
+			{
+				_matrix[i][ctemp3] = ++temp;
+			}
+			differ += 1;
+			ctemp++;
+			rtemp++;
+			count--;
+		}
+	}
+	else
+	{
+		int count = _row / 2 + 1;
+		while (count != 0)
+		{
+			_matrix[rtemp][ctemp] = num;
+			_matrix[rtemp + 1][ctemp] = final_num;
+			temp = num;
+			num = final_num + 1;
+			final_num = final_num - temp - 8 + num;
+
+			for (int i = ctemp + 1; i < _row - differ; i++)
+			{
+				_matrix[rtemp][i] = ++temp;
+			}
+
+			int ctemp2 = _row - differ - 1;
+			for (int i = rtemp + 1; i < _row - differ; i++)
+			{
+				_matrix[i][ctemp2] = ++temp;
+			}
+
+			int rtemp2 = _row - differ - 1;
+			for (int i = ctemp2 - 1; i >_row - rtemp2 - 2; i--)
+			{
+				_matrix[rtemp2][i] = ++temp;
+			}
+
+			int ctemp3 = _row - rtemp2 - 1;
+			for (int i = rtemp2 - 1; i > _row - rtemp2 - 1; i--)
+			{
+				_matrix[i][ctemp3] = ++temp;
+			}
+			differ += 1;
+			ctemp++;
+			rtemp++;
+			count--;
 		}
 	}
 }
