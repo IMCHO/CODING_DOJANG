fafaaca0aa8eb49587cd33fee4bec8a0ceb4c628
인택 조 <dlsxor21c@naver.com>
2018-01-28 12:28:26 +0900

    v0.2(catch misprint in odd and code compaction)

--- STEP_2/STEP_2/main.cpp
+++ STEP_2/STEP_2/main.cpp
@@ -37,85 +37,47 @@ void SpiralM(int * _matrix[], int _row, int _column)
 {
 	int num = 0;
 	int temp = 0;
-	int differ = 0;
+	int differ = 1;
 	int final_num = _row * 4 - 5;
 	int ctemp = 0 , rtemp = 0;
-	if (_row % 2 == 0)
+	int count;
+
+	if (_row % 2 == 0) count = _row / 2;                      // if n is even, n & n-1 rotate n/2
+	else count = _row / 2 + 1;
+
+	while (count != 0)
 	{
-		int count = _row / 2;
-		while (count != 0)
+		_matrix[rtemp][ctemp] = num;
+		if (count != 1) _matrix[rtemp + 1][ctemp] = final_num;       // forbid initialize final final_num in odd
+		temp = num;
+		num = final_num + 1;
+		final_num = final_num - temp - 8 + num;
+
+		for (int i = ctemp+1; i <= _column - differ; i++)    // increase column
 		{
-			_matrix[rtemp][ctemp] = num;
-			_matrix[rtemp + 1][ctemp] = final_num;
-			temp = num;
-			num = final_num + 1;
-			final_num = final_num - temp - 8 + num;
+			_matrix[rtemp][i] = ++temp;
+		}
 
-			for (int i = ctemp+1; i < _row - differ; i++)
-			{
-				_matrix[rtemp][i] = ++temp;
-			}
+		int ctemp2 = _column - differ;                        // column fix
+		for (int i = rtemp+1; i <= _row-differ; i++)         // increase row
+		{
+			_matrix[i][ctemp2] = ++temp;
+		}
 
-			int ctemp2 = _row - differ-1;
-			for (int i = rtemp+1; i < _row-differ; i++)
-			{
-				_matrix[i][ctemp2] = ++temp;
-			}
-
-			int rtemp2 = _row - differ - 1;
-			for (int i = ctemp2 - 1; i >_row-rtemp2-2 ; i--)
-			{
-				_matrix[rtemp2][i] = ++temp;
-			}
+		int rtemp2 = _row - differ;                       // row fix
+		for (int i = ctemp2 - 1; i >= _column - ctemp2 - 1; i--) // decrease column
+		{
+			_matrix[rtemp2][i] = ++temp;
+		}
 			
-			int ctemp3 = _row - rtemp2 - 1;
-			for (int i = rtemp2-1; i > _row-rtemp2-1; i--)
-			{
-				_matrix[i][ctemp3] = ++temp;
-			}
-			differ += 1;
-			ctemp++;
-			rtemp++;
-			count--;
-		}
-	}
-	else
-	{
-		int count = _row / 2 + 1;
-		while (count != 0)
+		int ctemp3 = _column - ctemp2 - 1;                       // column fix
+		for (int i = rtemp2 - 1; i >= _row - rtemp2; i--)       // decrease row
 		{
-			_matrix[rtemp][ctemp] = num;
-			_matrix[rtemp + 1][ctemp] = final_num;
-			temp = num;
-			num = final_num + 1;
-			final_num = final_num - temp - 8 + num;
-
-			for (int i = ctemp + 1; i < _row - differ; i++)
-			{
-				_matrix[rtemp][i] = ++temp;
-			}
-
-			int ctemp2 = _row - differ - 1;
-			for (int i = rtemp + 1; i < _row - differ; i++)
-			{
-				_matrix[i][ctemp2] = ++temp;
-			}
-
-			int rtemp2 = _row - differ - 1;
-			for (int i = ctemp2 - 1; i >_row - rtemp2 - 2; i--)
-			{
-				_matrix[rtemp2][i] = ++temp;
-			}
-
-			int ctemp3 = _row - rtemp2 - 1;
-			for (int i = rtemp2 - 1; i > _row - rtemp2 - 1; i--)
-			{
-				_matrix[i][ctemp3] = ++temp;
-			}
-			differ += 1;
-			ctemp++;
-			rtemp++;
-			count--;
+			_matrix[i][ctemp3] = ++temp;
 		}
+		differ += 1;
+		ctemp++;
+		rtemp++;
+		count--;
 	}
 }
