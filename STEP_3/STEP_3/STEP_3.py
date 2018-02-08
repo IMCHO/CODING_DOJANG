7efb5229080984e15880ee087582690934516390
인택 조 <dlsxor21c@naver.com>
2018-02-08 16:37:20 +0900

    1 & 2 complete, but need arrangement!

--- STEP_3/STEP_3/STEP_3.py
+++ STEP_3/STEP_3/STEP_3.py
@@ -1,42 +1,57 @@
+def printnum(list,size,string):
+    for i in range(2*size+3):
+        for j in range((size+3)*len(str(string))-1):
+                 print(list[i][j],end='')
+        print('')
 
-def one(size=1):
-    temp=size
-    print(' '*(size+2))
-    while temp!=0:
-        print(' '*(size+1)+'|')
-        temp-=1
-    print(' '*(size+2))
-    temp=size
-    while temp!=0 :
-        print(' '*(size+1)+'|')
-        temp-=1
-    print(' '*(size+2))
+def one(list,r,size):
+    for i in range(2*size+3):
+        if i==0 or i==(2*size+3)//2 or i==2*size+2: continue
+        else:
+            list[i][r+size+1]='|'
 
-def two(size=1):
-    print(' '+'-'*(size)+' ')
-    temp=size
-    while temp!=0:
-        print(' '*(size+1)+'|')
-        temp-=1
-    print(' '+'-'*(size)+' ')
-    temp=size
-    while temp!=0:
-        print('|'+' '*(size+1))
-        temp-=1
-    print(' '+'-'*(size)+' ')
+def two(list,r,size):
+    temp=r
+    limit=r+size+1
+    for i in range(2*size+3):
+        if i==0 or i==(2*size+3)//2 or i==2*size+2:
+            while temp!=limit+1:
+                if temp==r or temp==limit: temp+=1
+                else: 
+                    list[i][temp]='-'
+                    temp+=1
+        elif i>0 and i<(2*size+3)//2:
+            while temp!=limit+1:
+                if temp==limit: 
+                    list[i][temp]='|'
+                    temp+=1
+                else: temp+=1
+        else:
+            while temp!=limit+1:
+                if temp==r: 
+                    list[i][temp]='|'
+                    temp+=1
+                else: temp+=1
+        temp=r
 
-def three(size=1):
-    print(' '+'-'*(size)+' ')
-    temp=size
-    while temp!=0:
-        print(' '*(size+1)+'|')
-        temp-=1
-    print(' '+'-'*(size)+' ')
-    temp=size
-    while temp!=0:
-        print(' '*(size+1)+'|')
-        temp-=1
-    print(' '+'-'*(size)+' ')
-one(2)
-two(2)
-three(2)
+while True:
+    info=list(map(int,input().split()))
+    if info[0]==0: break
+    num=[[' ']*((info[0]+3)*len(str(info[1]))-1) for i in range(2*info[0]+3)]
+    row,col=0,0
+
+    for i in str(info[1]):
+        if i=='1': 
+            one(num,row,info[0])
+            row+=(3+info[0])
+        elif i=='2':
+            two(num,row,info[0])
+            row+=(3+info[0])
+        elif i=='3': pass
+        elif i=='4': pass
+        elif i=='5': pass
+        elif i=='6': pass
+        elif i=='7': pass
+        elif i=='8': pass
+        elif i=='9': pass
+    printnum(num,info[0],info[1])
